a9555f7b7f44f4dbcca00d5aa30bed52d9545b95
arthur <arthur32810@gmail.com>
2025-03-15 18:47:14 +0100

    exo6 fini

--- Exercice06/main.py
+++ Exercice06/main.py
@@ -1,5 +1,8 @@
 # Fonction calculate_average
- 
+def calculate_average(nombres):
+    return sum(nombres) / len(nombres)
+
+
 # Exemple d'utilisation de la fonction
 numbers = [10, 20, 30, 40, 50]
 average = calculate_average(numbers)
