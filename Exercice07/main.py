34cc4dcc860ddba5c593f54dd8c3d5963cf92853
arthur <arthur32810@gmail.com>
2025-03-15 18:59:34 +0100

    exo7 fini

--- Exercice07/main.py
+++ Exercice07/main.py
@@ -1,1 +1,12 @@
 ## Écrivez votre code ici !
+def square(nombre):
+    if nombre.isdigit():
+        return int(nombre) ** 2
+    else:
+        print("Le paramètre n'est pas un nombre !")
+        return None
+
+
+number = input("Saisissez un nombre :")
+result = square(number)
+print(result)
