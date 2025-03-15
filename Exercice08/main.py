5e66db44885c5e555d66bb2b51d9b2f682e69aaf
arthur <arthur32810@gmail.com>
2025-03-15 19:02:36 +0100

    exo8 fini

--- Exercice08/main.py
+++ Exercice08/main.py
@@ -1,8 +1,15 @@
 def log_decorator(func):
-     pass
- 
+    def wrapper():
+        print("Message avant l'appel de la fonction.")
+        func()
+        print("Message après l'appel de la fonction.")
+
+    return wrapper
+
+
 @log_decorator
 def function_test():
     print("Cette fonction ne prend pas d'arguments.")
+
 
 function_test()
