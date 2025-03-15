171419d4d16c8fb3558358c5dd4cf27f18602c72
arthur <arthur32810@gmail.com>
2025-03-15 19:15:23 +0100

    exo10 fini

--- Exercice10/main.py
+++ Exercice10/main.py
@@ -1,1 +1,24 @@
 ## Écrivez votre code ici !
+class Person:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_details(self):
+        return f"Je m'appele {self.name} et j'ai {self.age} ans !"
+
+
+class Employee(Person):
+
+    def __init__(self, name, age, salary):
+        super().__init__(name, age)
+        self.salary = salary
+
+    def display_details(self):
+        base = super().display_details()
+        return base + f" et je gagne {self.salary}€ par mois"
+
+
+employee = Employee("arthur", "25", 2000)
+print(employee.display_details())
