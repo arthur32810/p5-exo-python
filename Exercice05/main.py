38b7710115ac035d66acf3940ec12ce1c08e01da
arthur <arthur32810@gmail.com>
2025-03-15 18:40:26 +0100

    exo5fait

--- Exercice05/main.py
+++ Exercice05/main.py
@@ -1,5 +1,19 @@
 def sum(a, b):
+    """Somme de deux nombres
+    Args:
+        a (int): premier nombre
+        b (int): deuxième nombre
+    Returns:
+        int : somme de a et b
+    """
     return a + b
- 
+
+
 def subtraction(a, b):
+    """Soustraction de deux nombres
+    Args:
+        a (int): premier nombre
+        b (int): deuxième nombre
+    Returns:
+        int : soustraction de a et b"""
     return a - b
