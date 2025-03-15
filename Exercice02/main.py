ee08352d0c75ec571c7c69ad844098e338f243ef
arthur <arthur32810@gmail.com>
2025-03-15 14:39:56 +0100

    exo 2 fini !

--- Exercice02/main.py
+++ Exercice02/main.py
@@ -1,17 +1,19 @@
 students = {
-    'Alice': {
-         'Mathematiques': 90,
-         'Francais': 80,
-         'Histoire': 95
-    },
-    'Bob': {
-         'Mathematiques': 75,
-         'Francais': 85,
-         'Histoire': 70
-    },
-     'Charlie': {
-         'Mathematiques': 88,
-         'Francais': 92,
-         'Histoire': 78
-     }
+    "Alice": {"Mathematiques": 90, "Francais": 80, "Histoire": 95},
+    "Bob": {"Mathematiques": 75, "Francais": 85, "Histoire": 70},
+    "Charlie": {"Mathematiques": 88, "Francais": 92, "Histoire": 78},
 }
+
+student = input("Entrez le nom de l'étudiant: ")
+
+if student not in students:
+    print(f"L'étudiant {student} n'existe pas dans la liste.")
+else:
+    print(f"Notes de {student}:")
+    for subject, mark in students[student].items():
+        print(f"\t{subject}: {mark}")
+
+    print(
+        f"Moyenne de {student}: "
+        f"{sum(students[student].values()) / len(students[student])}"
+    )
